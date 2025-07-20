7dbdefac7c52fcaa9075eeb81cd5e3f176a35e19
Jesse Crocker <jesse@goatmaps.com>
2025-07-20 10:52:14 -0600

    Switch from prettier to biome (#2)

--- getMap.js
+++ getMap.js
@@ -1,147 +1,169 @@
 const debug = require("debug")("StaticMaps-gl.getMap");
 const genericPool = require("generic-pool");
 const maplibre = require("@maplibre/maplibre-gl-native");
-const fs = require("fs");
+const fs = require("node:fs");
 
-maplibre.on("message", function(e) {
-  debug("maplibre: ", e);
-  if (e.severity == "WARNING" || e.severity == "ERROR") {
-    console.log("maplibre:", e);
-  }
+maplibre.on("message", (e) => {
+	debug("maplibre: ", e);
+	if (e.severity === "WARNING" || e.severity === "ERROR") {
+		console.log("maplibre:", e);
+	}
 });
 
 function getMap() {
-  debug("Creating map");
-  var _map = new maplibre.Map({
-    ratio: 1.0,
-    request: function(req, callback) {
-      debug("request: " + JSON.stringify(req));
-      var start = Date.now();
-      var protocol = req.url.split(":")[0];
-      if (protocol == "file") {
-        var path = req.url.split("://")[1];
-        fs.readFile(path, function(err, data) {
-          if (err) {
-            return callback(err);
-          }
-          var response = {};
-          response.data = data;
-          callback(null, response);
-          debug("Request for " + req.url + " complete in " + (Date.now() - start) + "ms");
-        });
-      } else if (protocol === "http" || protocol === "https") {
-        // Add proper headers for tile requests
-        const headers = {
-          'User-Agent': 'StaticMaps-gl/1.0',
-          'Accept': '*/*',
-          'Accept-Encoding': 'gzip, deflate'
-        };
-        
-        // Use dynamic import for node-fetch 3.x
-        import('node-fetch').then(({ default: fetch }) => {
-          fetch(req.url, {
-            headers: headers,
-            timeout: 10000
-          })
-          .then(res => {
-            const duration = Date.now() - start;
-            if (duration > 500) {
-              debug(
-                "Request for " +
-                  req.url +
-                  " complete in " +
-                  duration +
-                  "ms.  Status:" +
-                  res.status
-              );
-            } else {
-              debug("Request for " + req.url + " complete in " + duration + "ms");
-            }
-            
-            if (res.ok) {
-              return res.buffer().then(body => {
-                var response = {};
-                if (res.headers.get('last-modified')) {
-                  response.modified = new Date(res.headers.get('last-modified'));
-                }
-                if (res.headers.get('expires')) {
-                  response.expires = new Date(res.headers.get('expires'));
-                }
-                if (res.headers.get('etag')) {
-                  response.etag = res.headers.get('etag');
-                }
-                response.data = body;
-                callback(null, response);
-              });
-            } else {
-              debug("Request failed with status " + res.status + " for " + req.url);
-              return callback(null, {});
-            }
-          })
-          .catch(err => {
-            debug("Request error for " + req.url + ": " + err.message);
-            callback(err);
-          });
-        }).catch(err => {
-          debug("Failed to import node-fetch: " + err.message);
-          callback(err);
-        });
-      } else {
-        debug(`request for invalid url: "${req.url}"`);
-        return callback(`request for invalid url: "${req.url}"`);
-      }
-    }
-  });
-  return _map;
+	debug("Creating map");
+	var _map = new maplibre.Map({
+		ratio: 1.0,
+		request: (req, callback) => {
+			debug(`request: ${JSON.stringify(req)}`);
+			var start = Date.now();
+			var protocol = req.url.split(":")[0];
+			var path;
+			if (protocol === "file") {
+				path = req.url.split("://")[1];
+				fs.readFile(path, (err, data) => {
+					if (err) {
+						return callback(err);
+					}
+					var response = {};
+					response.data = data;
+					callback(null, response);
+					debug(
+						"Request for " +
+							req.url +
+							" complete in " +
+							(Date.now() - start) +
+							"ms",
+					);
+				});
+			} else if (protocol === "http" || protocol === "https") {
+				// Add proper headers for tile requests
+				const headers = {
+					"User-Agent": "StaticMaps-gl/1.0",
+					Accept: "*/*",
+					"Accept-Encoding": "gzip, deflate",
+				};
+
+				// Use dynamic import for node-fetch 3.x
+				import("node-fetch")
+					.then(({ default: fetch }) => {
+						fetch(req.url, {
+							headers: headers,
+							timeout: 10000,
+						})
+							.then((res) => {
+								const duration = Date.now() - start;
+								if (duration > 500) {
+									debug(
+										"Request for " +
+											req.url +
+											" complete in " +
+											duration +
+											"ms.  Status:" +
+											res.status,
+									);
+								} else {
+									debug(
+										"Request for " +
+											req.url +
+											" complete in " +
+											duration +
+											"ms",
+									);
+								}
+
+								if (res.ok) {
+									return res.buffer().then((body) => {
+										var response = {};
+										if (res.headers.get("last-modified")) {
+											response.modified = new Date(
+												res.headers.get("last-modified"),
+											);
+										}
+										if (res.headers.get("expires")) {
+											response.expires = new Date(res.headers.get("expires"));
+										}
+										if (res.headers.get("etag")) {
+											response.etag = res.headers.get("etag");
+										}
+										response.data = body;
+										callback(null, response);
+									});
+								} else {
+									debug(
+										"Request failed with status " +
+											res.status +
+											" for " +
+											req.url,
+									);
+									return callback(null, {});
+								}
+							})
+							.catch((err) => {
+								debug(`Request error for ${req.url}: ${err.message}`);
+								callback(err);
+							});
+					})
+					.catch((err) => {
+						debug(`Failed to import node-fetch: ${err.message}`);
+						callback(err);
+					});
+			} else {
+				debug(`request for invalid url: "${req.url}"`);
+				return callback(`request for invalid url: "${req.url}"`);
+			}
+		},
+	});
+	return _map;
 }
 
 exports.getMap = getMap;
-exports.getMapPool = function() {
-  const factory = {
-    create: function() {
-      return new Promise(function(resolve, reject) {
-        try {
-          var map = getMap();
-          resolve(map);
-        } catch (err) {
-          console.error("Error creating map:", err);
-          reject(err);
-        }
-      });
-    },
-    destroy: function(resource) {
-      return new Promise(function(resolve) {
-        debug("Destroying map, used " + resource.useCount + " times.");
-        resource.release();
-        resolve();
-      });
-    }
-  };
+exports.getMapPool = () => {
+	const factory = {
+		create: () => {
+			var map;
+			return new Promise((resolve, reject) => {
+				try {
+					map = getMap();
+					resolve(map);
+				} catch (err) {
+					console.error("Error creating map:", err);
+					reject(err);
+				}
+			});
+		},
+		destroy: (resource) =>
+			new Promise((resolve) => {
+				debug(`Destroying map, used ${resource.useCount} times.`);
+				resource.release();
+				resolve();
+			}),
+	};
 
-  const maxMapUses = 0;
-  if (maxMapUses > 0) {
-    factory["validate"] = function(resource) {
-      debug("validate");
-      return new Promise(function(resolve) {
-        console.log("validate: usecount:" + resource.useCount);
-        if (resource.useCount != undefined && resource.useCount > maxMapUses) {
-          resolve(false);
-        } else {
-          resolve(true);
-        }
-      });
-    };
-  }
+	const maxMapUses = 0;
+	if (maxMapUses > 0) {
+		factory.validate = (resource) => {
+			debug("validate");
+			return new Promise((resolve) => {
+				console.log(`validate: usecount:${resource.useCount}`);
+				if (resource.useCount !== undefined && resource.useCount > maxMapUses) {
+					resolve(false);
+				} else {
+					resolve(true);
+				}
+			});
+		};
+	}
 
-  const opts = {
-    max: 20, // maximum size of the pool
-    min: 0, // minimum size of the pool
-    testOnBorrow: maxMapUses > 0,
-    idleTimeoutMillis: 15 * 60 * 1000,
-    evictionRunIntervalMillis: maxMapUses > 0 ? 5 * 60 * 1000 : 0
-  };
+	const opts = {
+		max: 20, // maximum size of the pool
+		min: 0, // minimum size of the pool
+		testOnBorrow: maxMapUses > 0,
+		idleTimeoutMillis: 15 * 60 * 1000,
+		evictionRunIntervalMillis: maxMapUses > 0 ? 5 * 60 * 1000 : 0,
+	};
 
-  debug("Creating map pool with opts:", opts);
-  const mapPool = genericPool.createPool(factory, opts);
-  return mapPool;
+	debug("Creating map pool with opts:", opts);
+	const mapPool = genericPool.createPool(factory, opts);
+	return mapPool;
 };
--- imageUtils.js
+++ imageUtils.js
@@ -1,95 +1,95 @@
 const debug = require("debug")("StaticMaps-gl.imageUtils");
 const sharp = require("sharp");
 
-debug("simd available: " + sharp.simd(true));
+debug(`simd available: ${sharp.simd(true)}`);
 
-exports.parseImageFormat = function(format) {
-  var imageFormat;
-  var mimetype;
-  var imageOptions = {};
+exports.parseImageFormat = (format) => {
+	var imageFormat;
+	var mimetype;
+	var imageOptions = {};
 
-  if (format.startsWith("png")) {
-    mimetype = "image/png";
-    imageFormat = "png";
-  } else if (format.startsWith("jpeg") || format.startsWith("jpg")) {
-    mimetype = "image/jpeg";
-    imageFormat = "jpeg";
-  } else if (format.startsWith("webp")) {
-    mimetype = "image/webp";
-    imageFormat = "webp";
-  } else {
-    return undefined;
-  }
+	if (format.startsWith("png")) {
+		mimetype = "image/png";
+		imageFormat = "png";
+	} else if (format.startsWith("jpeg") || format.startsWith("jpg")) {
+		mimetype = "image/jpeg";
+		imageFormat = "jpeg";
+	} else if (format.startsWith("webp")) {
+		mimetype = "image/webp";
+		imageFormat = "webp";
+	} else {
+		return undefined;
+	}
 
-  if (imageFormat == "jpeg" || imageFormat == "webp") {
-    if (format.endsWith("70")) {
-      imageOptions["quality"] = 70;
-    } else if (format.endsWith("80")) {
-      imageOptions["quality"] = 80;
-    } else if (format.endsWith("90")) {
-      imageOptions["quality"] = 90;
-    } else if (format.endsWith("100")) {
-      imageOptions["quality"] = 100;
-    }
-  } else if (imageFormat == "png") {
-    imageOptions["adaptiveFiltering"] = false;
-    imageOptions["progressive"] = false;
-    imageOptions["compressionLevel"] = 9;
-  }
+	if (imageFormat === "jpeg" || imageFormat === "webp") {
+		if (format.endsWith("70")) {
+			imageOptions.quality = 70;
+		} else if (format.endsWith("80")) {
+			imageOptions.quality = 80;
+		} else if (format.endsWith("90")) {
+			imageOptions.quality = 90;
+		} else if (format.endsWith("100")) {
+			imageOptions.quality = 100;
+		}
+	} else if (imageFormat === "png") {
+		imageOptions.adaptiveFiltering = false;
+		imageOptions.progressive = false;
+		imageOptions.compressionLevel = 9;
+	}
 
-  return {
-    format: imageFormat,
-    mimetype,
-    options: imageOptions
-  };
+	return {
+		format: imageFormat,
+		mimetype,
+		options: imageOptions,
+	};
 };
 
-exports.sendImageResponse = function(res, width, height, data, imageFormat) {
-  const start = Date.now();
-  
-  // Un-premultiply pixel values
-  // MapLibre GL buffer contains premultiplied values, which are not handled correctly by sharp
-  // since we are dealing with 8-bit RGBA values, normalize alpha onto 0-255 scale and divide
-  // it out of RGB values
-  for (let i = 0; i < data.length; i += 4) {
-    const alpha = data[i + 3];
-    const norm = alpha / 255;
-    if (alpha === 0) {
-      data[i] = 0;
-      data[i + 1] = 0;
-      data[i + 2] = 0;
-    } else {
-      data[i] /= norm;
-      data[i + 1] = data[i + 1] / norm;
-      data[i + 2] = data[i + 2] / norm;
-    }
-  }
+exports.sendImageResponse = (res, width, height, data, imageFormat) => {
+	const start = Date.now();
 
-  const image = sharp(data, {
-    raw: {
-      width: width,
-      height: height,
-      channels: 4
-    }
-  });
+	// Un-premultiply pixel values
+	// MapLibre GL buffer contains premultiplied values, which are not handled correctly by sharp
+	// since we are dealing with 8-bit RGBA values, normalize alpha onto 0-255 scale and divide
+	// it out of RGB values
+	for (let i = 0; i < data.length; i += 4) {
+		const alpha = data[i + 3];
+		const norm = alpha / 255;
+		if (alpha === 0) {
+			data[i] = 0;
+			data[i + 1] = 0;
+			data[i + 2] = 0;
+		} else {
+			data[i] /= norm;
+			data[i + 1] = data[i + 1] / norm;
+			data[i + 2] = data[i + 2] / norm;
+		}
+	}
 
-  var formattedImage;
-  if (imageFormat.format == "png") {
-    formattedImage = image.png(imageFormat.options);
-  } else if (imageFormat.format == "jpeg") {
-    formattedImage = image.jpeg(imageFormat.options);
-  } else if (imageFormat.format == "webp") {
-    formattedImage = image.webp(imageFormat.options);
-  }
+	const image = sharp(data, {
+		raw: {
+			width: width,
+			height: height,
+			channels: 4,
+		},
+	});
 
-  formattedImage.toBuffer(function(err, data, info) {
-    if (err) {
-      debug("Error saving image: " + err);
-      res.status(500).send("Error saving image");
-      return;
-    }
-    debug("Saving image complete in " + (Date.now() - start) + "ms");
-    res.type(imageFormat.mimetype);
-    res.send(data);
-  });
+	var formattedImage;
+	if (imageFormat.format === "png") {
+		formattedImage = image.png(imageFormat.options);
+	} else if (imageFormat.format === "jpeg") {
+		formattedImage = image.jpeg(imageFormat.options);
+	} else if (imageFormat.format === "webp") {
+		formattedImage = image.webp(imageFormat.options);
+	}
+
+	formattedImage.toBuffer((err, data, _info) => {
+		if (err) {
+			debug(`Error saving image: ${err}`);
+			res.status(500).send("Error saving image");
+			return;
+		}
+		debug(`Saving image complete in ${Date.now() - start}ms`);
+		res.type(imageFormat.mimetype);
+		res.send(data);
+	});
 };
